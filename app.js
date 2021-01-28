23ebd57244d949ba3174983bb0badb4e1fd0e547
bono13 <cornejo.dev@gmail.com>
2021-01-28 19:08:13 +0300

    Create user and save to db

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
+require('./db/server');
+const User = require('./models/user');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-	res.json('Sanity check...');
+	try {
+		res.json('Sanity check...');
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.post('/', async (req, res) => {
+	const user = new User(req.body);
+	try {
+		await user.save();
+		// res.json('Sanity check...');
+		res.redirect('/success');
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.get('/success', (req, res) => {
+	try {
+		res.json('created user');
+	} catch (err) {}
 });
 
 app.listen(port, () => {
